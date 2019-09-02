8a7a958af2dbd5e31327c7ab7629f4add6f16a33
Ravi Chandra <Ravi Chandra@LAPTOP-B9FOBDT5>
2019-09-03 01:12:44 +0530

    DI/IOC in EntityFramework Core

--- RESTApisDemo/Controllers/ProductsController.cs
+++ RESTApisDemo/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RESTApisDemo.Data;
 using RESTApisDemo.Models;
+using RESTApisDemo.Services;
 
 namespace RESTApisDemo.Controllers
 {
@@ -13,44 +14,25 @@ namespace RESTApisDemo.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private ProductsDbContext _productsDbContext;
-
-        public ProductsController(ProductsDbContext productsDbContext)
+        private IProduct _productRepository;
+        public ProductsController(IProduct productRepository)
         {
-            _productsDbContext = productsDbContext;
+            _productRepository = productRepository;
         }
         // GET: api/Products
         //https://localhost:44399/api/products?pageNumber=2&pageSize=5
+        //https://localhost:44399/api/products?searchProduct=sam with header key ="Accept" & Value ="application/json;v=1.0" and searchProduct="mob"
         [HttpGet]
         public IEnumerable<Product> Get(string sortDesc,int? pageNumber,int? pageSize,string searchProduct)
         {
-            int currentPage = pageNumber ?? 1;
-            int currentPageSize = pageSize ?? 5;
-            
-            IQueryable<Product> products = _productsDbContext.Products.Where(x => x.ProductName.StartsWith(searchProduct)); //1. search Implementation
-            
-            switch (sortDesc) //2. sort Implementation
-            {
-                case "desc":
-                    products = products.OrderByDescending(x => int.Parse(x.ProductPrice));
-                    break;
-                case "asc":
-                    products = products.OrderBy(x => int.Parse(x.ProductPrice));
-                    break;
-                default:
-                    break;
-            }
-            
-            var productItems = products.Skip((currentPage - 1) * currentPageSize).Take(currentPageSize).ToList(); //3. pagination
-
-            return productItems;
+            return _productRepository.GetProducts(sortDesc, pageNumber, pageSize, searchProduct);
         }
 
         // GET: api/Products/5
         [HttpGet("{id}", Name = "Get")]
         public IActionResult Get(int id)
         {
-            var product = _productsDbContext.Products.SingleOrDefault(x => x.ProductId == id);
+            var product = _productRepository.GetProduct(id);
             if(product == null)
             {
                 return NotFound("No Record Found.....");
@@ -68,8 +50,8 @@ namespace RESTApisDemo.Controllers
                 return BadRequest(ModelState);
             }
 
-            _productsDbContext.Products.Add(product);
-            _productsDbContext.SaveChanges(true);
+            _productRepository.AddProduct(product);
+           
             return StatusCode(StatusCodes.Status201Created);
         }
 
@@ -90,8 +72,7 @@ namespace RESTApisDemo.Controllers
 
             try
             {
-                _productsDbContext.Products.Update(product);
-                _productsDbContext.SaveChanges(true);
+                _productRepository.UpdateProduct(product);
             }
             catch(Exception)
             {
@@ -105,14 +86,7 @@ namespace RESTApisDemo.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var product = _productsDbContext.Products.SingleOrDefault(x => x.ProductId == id);
-            if (product == null)
-            {
-                return NotFound("No Record Found.....");
-            }
-
-            _productsDbContext.Products.Remove(product);
-            _productsDbContext.SaveChanges(true);
+            _productRepository.DeleteProduct(id);
             return Ok("Product Deleted...");
         }
     }
---
+++ RESTApisDemo/Services/ProductRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RESTApisDemo.Data;
+using RESTApisDemo.Models;
+
+namespace RESTApisDemo.Services
+{
+    public class ProductRepository : IProduct
+    {
+        private ProductsDbContext _productsDbContext;
+
+        public ProductRepository(ProductsDbContext productsDbContext)
+        {
+            _productsDbContext = productsDbContext;
+        }
+        public void AddProduct(Product product)
+        {
+            _productsDbContext.Products.Add(product);
+            _productsDbContext.SaveChanges(true);
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var product = _productsDbContext.Products.Find(id);
+            _productsDbContext.Products.Remove(product);
+            _productsDbContext.SaveChanges(true);
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _productsDbContext.Products.SingleOrDefault(x => x.ProductId == id);
+        }
+
+        public IEnumerable<Product> GetProducts(string sortDesc, int? pageNumber, int? pageSize, string searchProduct)
+        {
+            int currentPage = pageNumber ?? 1;
+            int currentPageSize = pageSize ?? 5;
+
+            IQueryable<Product> products = _productsDbContext.Products.Where(x => x.ProductName.StartsWith(searchProduct)); //1. search Implementation
+
+            switch (sortDesc) //2. sort Implementation
+            {
+                case "desc":
+                    products = products.OrderByDescending(x => int.Parse(x.ProductPrice));
+                    break;
+                case "asc":
+                    products = products.OrderBy(x => int.Parse(x.ProductPrice));
+                    break;
+                default:
+                    break;
+            }
+
+            var productItems = products.Skip((currentPage - 1) * currentPageSize).Take(currentPageSize).ToList(); //3. pagination
+
+            return productItems;
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _productsDbContext.Products.Update(product);
+            _productsDbContext.SaveChanges(true);
+        }
+    }
+}
